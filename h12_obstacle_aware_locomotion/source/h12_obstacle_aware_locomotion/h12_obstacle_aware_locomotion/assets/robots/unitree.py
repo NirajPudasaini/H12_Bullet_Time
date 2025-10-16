0cd2e1ee729e1cbc70d5b829f18c4aa68a746aaa
Niraj Pudasaini <np2289@nyu.edu>
2025-10-16 14:43:32 -0600

    added H12_handless specs

--- h12_obstacle_aware_locomotion/source/h12_obstacle_aware_locomotion/h12_obstacle_aware_locomotion/assets/robots/unitree.py
+++ h12_obstacle_aware_locomotion/source/h12_obstacle_aware_locomotion/h12_obstacle_aware_locomotion/assets/robots/unitree.py
@@ -2,14 +2,213 @@
 # License: Apache License, Version 2.0  
 """Configuration for Unitree robots."""
 
+import os
+
+project_root = "/home/niraj/isaac_projects/H12_Obstacle_Aware_Locomotion/h12_obstacle_aware_locomotion/source/h12_obstacle_aware_locomotion/h12_obstacle_aware_locomotion"
+
 import isaaclab.sim as sim_utils
 from isaaclab.actuators import ActuatorNetMLPCfg, DCMotorCfg, ImplicitActuatorCfg
 from isaaclab.assets.articulation import ArticulationCfg
 from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
-import os
-project_root = os.environ.get("PROJECT_ROOT")
+
+H12_CFG_HANDLESS = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{project_root}/assets/robots/unitree_model/H1-2/h1_2_handless/h1_2_handless.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, 
+            solver_position_iteration_count=8,
+            solver_velocity_iteration_count=4
+        ),
+
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 1.05),               #same as gym
+        joint_pos={
+            # legs joints
+            "left_hip_yaw_joint": 0.0,
+            "left_hip_roll_joint": 0.0,
+            "left_hip_pitch_joint": -0.16, #same as gym
+            "left_knee_joint": 0.36,    #same as gym
+            "left_ankle_pitch_joint": -0.15, #gym is -0.2,reduced a bit to avoid foot collision with ground
+            "left_ankle_roll_joint": 0.0,
+            
+            "right_hip_yaw_joint": 0.0,
+            "right_hip_roll_joint": 0.0,
+            "right_hip_pitch_joint": -0.16,
+            "right_knee_joint": 0.36,
+            "right_ankle_pitch_joint": -0.15,
+            "right_ankle_roll_joint": 0.0,
+            
+            
+            # arms joints
+            "left_shoulder_pitch_joint": 0.4, # ~ 23 degrees, same as gym
+            "left_shoulder_roll_joint": 0.0,
+            "left_shoulder_yaw_joint": 0.0,
+            "left_elbow_joint": 0.3,           # ~ 14.1 degrees, same as gym
+            "left_wrist_roll_joint": 0.0,
+            "left_wrist_pitch_joint": 0.0,
+            "left_wrist_yaw_joint": 0.0,
+            
+            "right_shoulder_pitch_joint": 0.4,  # ~ 23 degrees, same as gym
+            "right_shoulder_roll_joint": 0.0,
+            "right_shoulder_yaw_joint": 0.0,
+            "right_elbow_joint": 0.3,        # ~ 14.1 degrees, same as gym
+            "right_wrist_roll_joint": 0.0,
+            "right_wrist_pitch_joint": 0.0,
+            "right_wrist_yaw_joint": 0.0,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
 
 
+    # actuators={
+    #     "legs": ImplicitActuatorCfg(
+    #         joint_names_expr=[
+    #             ".*_hip_yaw_joint", 
+    #             ".*_hip_roll_joint",
+    #             ".*_hip_pitch_joint", 
+    #             ".*_knee_joint",
+    #         ],
+    #         effort_limit=300,
+    #         velocity_limit=100,
+    #         stiffness=None,
+    #         damping=None,
+    #         armature=None,
+    #     ),
+    #     "feet": ImplicitActuatorCfg(
+    #         effort_limit=None,
+    #         joint_names_expr=[".*_ankle_pitch_joint", ".*_ankle_roll_joint"],
+    #         stiffness=None,
+    #         damping=None,
+    #         # armature=0.001,
+    #     ),
+    #     "arms": ImplicitActuatorCfg(
+    #         joint_names_expr=[
+    #             ".*_shoulder_.*_joint",
+    #             ".*_elbow_joint",
+    #             ".*_wrist_.*_joint"
+    #         ],
+    #         effort_limit=None,
+    #         velocity_limit=None,
+    #          stiffness={  # increase the stiffness (kp)
+    #              ".*_shoulder_.*_joint": 25.0,
+    #              ".*_elbow_joint": 50.0,
+    #              ".*_wrist_.*_joint": 40.0,
+    #         },
+    #          damping={    # increase the damping (kd)
+    #              ".*_shoulder_.*_joint": 2.0,
+    #              ".*_elbow_joint": 2.0,
+    #              ".*_wrist_.*_joint": 2.0,
+    #          },
+    #         armature=None,
+    #     ),
+
+actuators={
+    # Motor: M107-24, Torque: 300 Nm
+    # From your original "legs" group
+    "hip_pitch_roll_knee": ImplicitActuatorCfg(
+        joint_names_expr=[
+            ".*_hip_pitch_joint", 
+            ".*_hip_roll_joint",
+            ".*_knee_joint"
+        ],
+        effort_limit=300,
+        velocity_limit=100,
+        stiffness={
+            ".*_hip_pitch_joint": 200.0,
+            ".*_hip_roll_joint": 200.0,
+            ".*_knee_joint": 300.0,
+        },
+        damping={
+            ".*_hip_pitch_joint": 2.5,
+            ".*_hip_roll_joint": 2.5,
+            ".*_knee_joint": 4.0,},
+        armature=None,
+    ),
+    # Motor: M107-15, Torque: 200 Nm
+    # From your original "legs" group
+    "hip_yaw": ImplicitActuatorCfg(
+        joint_names_expr=[".*_hip_yaw_joint"],
+        effort_limit=200,
+        velocity_limit=100,
+        stiffness={"^.*_hip_yaw_joint$": 200.0,},
+        damping={"^.*_hip_yaw_joint$": 2.5,},
+        armature=None,
+    ),
+    # Motor: N7520-22.5, Torque: 120 Nm
+    # From your original "arms" group
+    "shoulder_pitch_roll_and_elbow": ImplicitActuatorCfg(
+        joint_names_expr=[
+            ".*_shoulder_pitch_joint",
+            ".*_shoulder_roll_joint",
+            ".*_elbow_joint"
+        ],
+        effort_limit= 120,
+        velocity_limit= 100,
+        stiffness={
+            ".*_shoulder_.*_joint":80.0,
+            ".*_elbow_joint": 40.0,
+        },
+        damping={
+            ".*_shoulder_.*_joint": 2.0,
+            ".*_elbow_joint": 1.0,
+        },
+        armature=None,
+    ),
+    # Motor: N5020-16, Torque: 25 Nm
+    # From your original "arms" group
+    "wrists": ImplicitActuatorCfg(
+        joint_names_expr=[
+            ".*_wrist_.*_joint"
+        ],
+        effort_limit=25.0,
+        velocity_limit=100,
+        stiffness={
+            ".*_wrist_.*_joint": 60.0,
+        },
+        damping={
+            ".*_wrist_.*_joint": 0.5,
+        },
+        armature=None,
+    ),
+    # Motor: N7520-14.3, Torque: 75 Nm
+    # Combined from your "feet" and "arms" groups
+    "ankles_and_shoulder_yaw": ImplicitActuatorCfg(
+        joint_names_expr=[
+            ".*_ankle_pitch_joint",
+            ".*_ankle_roll_joint",
+            ".*_shoulder_yaw_joint"
+        ],
+        effort_limit=75,
+        velocity_limit=100,
+        stiffness={
+            ".*_ankle_pitch_joint": 60.0,      # From original "feet" config
+            ".*_ankle_roll_joint": 40.0,      # From original "feet" config
+            ".*_shoulder_yaw_joint": 40.0, # From original "arms" config
+        },
+        damping={
+            ".*_ankle_pitch_joint": 1.0,       # From original "feet" config
+            ".*_ankle_roll_joint": 0.3,       # From original "feet" config
+            ".*_shoulder_yaw_joint": 1.0,   # From original "arms" config
+        },
+        armature=None,
+    ),
+    },
+)
+
+
+#THE FOLLOWING NEEDS EDITING AND TUNING
 H12_CFG_WITH_INSPIRE_HAND = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
         usd_path=f"{project_root}/assets/robots/h1_2-26dof-inspire-base-fix-usd/h1_2_26dof_with_inspire_rev_1_0.usd",
